a8307f31a8361d7b3f1d413f5f921b2a18ac179f
Naveenvk29 <naveenvinodkumar29@gmail.com>
2024-02-27 16:12:54 +0530

    first commit

---
+++ Todo/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todoes'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>redex</h1>
+      <AddTodo/>
+      <Todos/>
+    </>
+  )
+}
+
+export default App
